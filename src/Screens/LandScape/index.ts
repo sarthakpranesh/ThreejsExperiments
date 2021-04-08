798aad56eabb81813a2898060e088fb598f1b55a
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-04-08 14:31:10 +0530

    Completed Soldier model
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- src/Screens/LandScape/index.ts
+++ src/Screens/LandScape/index.ts
@@ -1,5 +1,4 @@
-import { PerspectiveCamera, HemisphereLight, DirectionalLight, Scene, WebGLRenderer, Mesh, PlaneGeometry, MeshBasicMaterial, DoubleSide } from 'three';
-import {FirstPersonControls} from '../../lib/FirstPersonControls.js';
+import { PerspectiveCamera, HemisphereLight, DirectionalLight, Scene, WebGLRenderer, Mesh, PlaneGeometry, MeshBasicMaterial, DoubleSide, MathUtils, Color } from 'three';
 import {OrbitControls} from '../../lib/OrbitControls.js';
 import {GLTFLoader} from '../../lib/GLTFLoader.js';
 
@@ -9,41 +8,20 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 // creating the camera
 const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1000);
-// update camera position
-camera.position.z = 10;
-camera.rotation.x = 90;
 // creating a scene
 const scene = new Scene();
+scene.background = new Color('#87CEEB');
 
 // defining a Orbit Controller
-// const orbit = new OrbitControls(camera, renderer.domElement);
-// orbit.update();
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
 
-// add a default light
-{
-    const skyColor = 0xB1E1FF;  // light blue
-    const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 1;
-    const light = new HemisphereLight(skyColor, groundColor, intensity);
-    scene.add(light);
-}
-{
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new DirectionalLight(color, intensity);
-    light.position.set(5, 10, 2);
-    scene.add(light);
-    scene.add(light.target);
-}
-
-// get a plane
-{
-    const plane = new Mesh(
-        new PlaneGeometry(100, 100, 10, 10),
-        new MeshBasicMaterial({ color: 'pink', side: DoubleSide }),
-    );
-    scene.add(plane);
-}
+// add hemisphere light
+const skyColor = 0xB1E1FF;  // light blue
+const groundColor = 0xB97A20;  // brownish orange
+const intensity = 1;
+const light = new HemisphereLight(skyColor, groundColor, intensity);
+scene.add(light);
 
 let gltfLoader = new GLTFLoader();
 const s = require('../../../assets/s.glb');
@@ -52,9 +30,17 @@ gltfLoader.load(s, (g) => {
     scene.add(sold);
 });
 
+// update camera position
+camera.position.z = 10;
+camera.position.y = 10;
+
+// adding info
+document.getElementById('info').innerText = 'Soldier Modal'
+
 function animate () {
     requestAnimationFrame(animate);
-    // orbit.update();
+
+    orbit.update();
     renderer.render(scene, camera);
 }
 
